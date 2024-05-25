f6b9edef6111db02958ead6adffb1eef604d18d3
Olivier <Olivier@DESKTOP-Q76BK1H>
2024-05-25 18:55:09 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorPizzas/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPizzas.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required] public string Name { get; set; }
+
+        [Required] public string[] Ingredients { get; set; }
+
+        [Range(1, 99)] public int Price { get; set; }
+
+        [Required] public string ImageName { get; set; }
+
+    }
+}
---
+++ BlazorPizzas/Services/IPizzaManager.cs
@@ -0,0 +1,14 @@
+﻿using BlazorPizzas.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorPizzas.Services
+{
+	public interface IPizzaManager
+	{
+		Task<IEnumerable<Pizza>> GetPizzas();
+
+		Task AddOrUpdate(Pizza p);
+	}
+}
